bfe408067b132cca69ded53decc6efa75f98b5e5
Kancha98 <emkanchana98@gmail.com>
2025-05-10 12:25:59 +0530

    Update CSE_Predictor_by_Maverick.py

--- pages/CSE_Predictor_by_Maverick.py
+++ pages/CSE_Predictor_by_Maverick.py
@@ -258,14 +258,14 @@ try:
             
             # Use .copy() to avoid SettingWithCopyWarning if tier_2_picks is a slice
             tier_2_picks_processed = tier_2_picks.copy()
+            tier_2_picks_processed = tier_2_picks_processed.reset_index(drop=True) 
             
-            # This step is important even if it looks like datetime, to be safe.
-            tier_2_picks_processed['date'] = pd.to_datetime(tier_2_picks_processed['date'])
 
             # Now format the datetime objects into strings with only the date part
-            tier_2_picks_processed['date'] = tier_2_picks_processed['date'].dt.strftime('%Y-%m-%d')
+            tier_2_picks_processed['date'] = pd.to_datetime(tier_2_picks_processed['date'], errors='coerce').dt.strftime('%Y-%m-%d')
+
             
-            tier_2_picks_processed = tier_2_picks_processed.reset_index(drop=True)  # Remove index
+             # Remove index
             
             column_rename_map_filtered = {
                  'date': 'Date',
